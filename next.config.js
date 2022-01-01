1f6c1eca9d831665b1d486a96887ce71b4613775
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-01-01 22:16:18 +0300

    initial commit
    Set up with this guide:
    https://gist.github.com/ChrisCrossCrash/d6d8e2bc2fcb73b02b7c73e35ea42abc

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    // The build process has its own tsconfig.json, which ignores the cypress directory.
+    tsconfigPath: './tsconfig.build.json',
+  },
 }
