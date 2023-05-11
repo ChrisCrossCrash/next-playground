488769750b39fc2d7a91e619fee58444b9a31ec8
Christopher Kumm <chrislkumm@protonmail.com>
2023-05-11 14:27:52 -0500

    Switch to app router

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.scss'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Home page description',
+}
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  )
+}
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <div className='centered'>TODO</div>
-}
-
-export default Home
